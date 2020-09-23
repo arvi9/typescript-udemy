6be4afeeb63169f6363fa7bbee5cfac6a2ecb3bd
adc work <adc9work@outlook.com>
2020-09-23 23:41:37 +0530

    Sorting DOne!

--- sort/src/Sorter.ts
+++ sort/src/Sorter.ts
@@ -6,19 +6,19 @@ interface Sortable {
   swap(leftIndex: number, rightIndex: number): void;
 }
 
-export class Sorter {
-  // collection: number[];
+export abstract class Sorter {
+  // constructor(public collection: Sortable) {}
+  abstract length: number;
+  abstract compare(leftIndex: number, rightIndex: number): boolean;
+  abstract swap(leftIndex: number, rightIndex: number): void;
 
-  constructor(public collection: Sortable) {
-    // this.collection = collection;
-  }
   sort(): void {
-    const { length } = this.collection;
+    const { length } = this;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection.compare(j, j + 1)) {
-          this.collection.swap(j, j + 1);
+        if (this.compare(j, j + 1)) {
+          this.swap(j, j + 1);
         }
       }
     }
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,14 +1,27 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 console.log("-----------------Number Sorter----------------------");
 const nuberCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(nuberCollection);
-sorter.sort();
-console.log(nuberCollection.data);
+nuberCollection.sort();
+// const sorter = new Sorter(nuberCollection);
+// sorter.sort();
+// console.log(nuberCollection.data);
 
 console.log("----------------Character Sorter-----------------------");
 const charactersCollection = new CharactersCollection("Xaab");
-const stringSorter = new Sorter(charactersCollection);
-stringSorter.sort();
+charactersCollection; // const stringSorter = new Sorter(charactersCollection);
+// stringSorter.sort();
+
+console.log("----------------Linked List Sorter-----------------------");
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+// const sorterList = new Sorter(linkedList);
+linkedList.sort();
+linkedList.print();
